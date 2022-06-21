251813fbf8afb005aa00f3a0f2b2d0a68f206879
shahadat13593 <shahadat15-13593@diu.edu.bd>
2022-06-22 01:03:18 +0600

    day1

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,1 +1,9 @@
-print(44)
+# Use of Backslash character
+print('Shahadat Hossain\nId: 201-15-13593')
+
+print('\n')
+# string concatenation
+# 3 way of adding space
+print('Shahadat' + " " + 'hossain')
+print('Shahadat ' + 'hossain')
+print('Shahadat' + ' hossain')
